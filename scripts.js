f65eefaa3f1a2e4c0b3201b877ef4658190e7108
Al Najath <alnajath1@gmail.com>
2024-07-16 12:10:55 +0530

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,21 @@
+// Smooth scroll for navigation links
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('nav ul li a');
+
+    for (const link of links) {
+        link.addEventListener('click', smoothScroll);
+    }
+
+    function smoothScroll(event) {
+        event.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop - document.querySelector('nav').offsetHeight,
+            behavior: 'smooth'
+        });
+    }
+});
+
